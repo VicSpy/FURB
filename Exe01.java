ee283d00a56fcd68022e031397413a97b7a128e9
VicSpy <98160719+VicSpy@users.noreply.github.com>
2022-05-09 20:45:28 -0300

    Add files via upload

---
+++ Exe01.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Exe01 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        for (int num = 1; num <= 10; num = num + 2) {
+            if (num % 2 == 0) {
+                System.out.println("É par");
+
+            } else {
+                System.out.println("É impar");
+            }
+        }
+
+        sc.close();
+    }
+}
