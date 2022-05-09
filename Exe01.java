7c21f5eb1e8409ca54831cfe9de747dab4f466c5
VicSpy <98160719+VicSpy@users.noreply.github.com>
2022-05-09 20:47:59 -0300

    Update Exe01.java

--- Exe01.java
+++ Exe01.java
@@ -4,7 +4,7 @@ public class Exe01 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        for (int num = 1; num <= 10; num = num + 2) {
+        for (int num = 1; num <= 20; num = num + 2) {
             if (num % 2 == 0) {
                 System.out.println("É par");
 
